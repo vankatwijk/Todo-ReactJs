bd13594588c988692c9753b47e5848b7426d2c9c
vankatwijk <hendrikus.hpvk@gmail.com>
2020-07-12 12:54:27 +0200

    listing out the todos in todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Todo from './Todo'
+
 
 export default function TodoList({todos}) {
     return (
-        <div>
-            {todos.length}
-        </div>
+        todos.map(todo =>{
+            return <Todo todo={todo} />
+        })
     )
 }
